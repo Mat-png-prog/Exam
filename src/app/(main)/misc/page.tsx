3dde42033c1473d4d9886838831862523bf2117b
Mat04 <stopglobalwarming622@gmail.com>
2024-12-05 10:08:26 +0000

    .............

--- src/app/(main)/misc/page.tsx
+++ src/app/(main)/misc/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Image1 from './image1';
 import Image2 from './image2';
-import file from "@/public/file.svg";
 
 const MiscPage = () => {
   return (
@@ -9,8 +8,7 @@ const MiscPage = () => {
       <Image1 />
       <h1>Hello</h1>
       <Image2 />
-      <br />
-      <file />
+
     </div>
   );
 };
